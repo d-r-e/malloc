dc5b88505684e770c3fb2fd09cee3cc3504afb9b
David Rodríguez Elipe <david.rodriguezelipe@telefonica.com>
2023-08-10 17:11:31 +0000

    PREALLOC

--- src/malloc.c
+++ src/malloc.c
@@ -4,99 +4,125 @@ t_block *g_head = NULL;
 
 /***
  * show_alloc_mem
- * This function is used to print the memory allocation of the preallocated blocks.
-*/
-void show_alloc_mem(){
-	t_block *tmp = NULL;
+ * This function is used to print the memory allocation of the pre-allocated blocks.
+ */
+void show_alloc_mem() {
+    t_block *tmp = g_head;
+    size_t offset;
 
-	tmp = g_head;
-	printf("TINY : %p\n", (void*)tmp);
-	while (tmp != NULL)
-	{
-		if (tmp->size == TINY)
-		{
-			printf("%p - %p : %lu bytes\n", (void*)(tmp + sizeof(t_block)), (void*)(tmp + sizeof(t_block) + TINY), tmp->size);
-		} else if (tmp->size == SMALL)
-		{
-			printf("%p - %p : %lu bytes\n", (void*)(tmp + sizeof(t_block)), (void*)(tmp + sizeof(t_block) + SMALL), tmp->size);
-		} else if (tmp->size == LARGE)
-		{
-			printf("%p - %p : %lu bytes\n", (void*)(tmp + sizeof(t_block)), (void*)(tmp + sizeof(t_block) + LARGE), tmp->size);
-		}
-		tmp = tmp->next;
+    printf("TINY : %p\n", (void *) tmp);
 
-	}
+    while (tmp != NULL) {
+        if (tmp->size == TINY) {
+            offset = TINY;
+        } else if (tmp->size == SMALL) {
+            offset = SMALL;
+        } else if (tmp->size == LARGE) {
+            offset = LARGE;
+        } else 
+            offset = 0;
+        
 
-	
+        printf("%p - %p : %lu bytes\n", 
+               (void *)((char *)tmp + sizeof(t_block)), 
+               (void *)((char *)tmp + sizeof(t_block) + offset), 
+               tmp->size);
+
+        tmp = tmp->next;
+    }
 }
 
-/***
- * prealloc
- * For the three predefined sizes, TINY, SMALL and LARGE, we preallocate 100 blocks of each size.
-*/
-static int prealloc(void)
-{
-	int i;
-	t_block *tmp = NULL;
 
-	if (g_head != NULL)
-		return 0;
-
-	// TINY
-	g_head = mmap(NULL, (sizeof(t_block) + TINY) * 100, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if (g_head == MAP_FAILED)
-		return -1;
-	tmp = g_head;
-	for (i = 0; i < 3; i++)
-	{
-		tmp->size = TINY;
-		tmp->inuse = false;
-		tmp->next = (t_block *)(tmp + sizeof(t_block) + TINY);
-		tmp->prev = i > 0 ? (t_block *)(tmp - sizeof(t_block) - TINY) : NULL;
-		if (i == 2)
-			tmp->next = NULL;
-		tmp = tmp->next;
-	}
-	show_alloc_mem();
-	return 0;
+static size_t calculate_total_memory(){
+	return (sizeof(t_block) + TINY) * N_BLOCKS + (sizeof(t_block) + SMALL) * N_BLOCKS + (sizeof(t_block) + LARGE) * N_BLOCKS;
 }
 
-void *ft_malloc(size_t size)
-{
-	void *ptr = NULL;
-	int page_size = getpagesize();
-	struct rlimit limit;
 
-	prealloc();
+static int prealloc(void) {
+    int i;
 
-// printf("page size: %d\n", page_size);
-	getrlimit(RLIMIT_AS, &limit);
-	if (size > limit.rlim_cur)
-		return NULL;
+    t_block *tmp = NULL;
+    size_t tiny_tblock = sizeof(t_block) + TINY;
+    size_t small_tblock = sizeof(t_block) + SMALL;
+
+    if (g_head != NULL)
+        return 0;
+
+    g_head = mmap(NULL, calculate_total_memory(), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (g_head == MAP_FAILED)
+        return -1;
+
+    // TINY
+    tmp = g_head;
+    for (i = 0; i < N_BLOCKS; i++) {
+        tmp->size = TINY;
+        tmp->inuse = false;
+        tmp->next = (t_block *)((char *)tmp + tiny_tblock);
+        tmp->prev = i > 0 ? (t_block *)((char *)tmp - tiny_tblock) : NULL;
+        tmp = tmp->next;
+    }
+    // SMALL
+    for (i = 0; i < N_BLOCKS; i++) {
+        tmp->size = SMALL;
+        tmp->inuse = false;
+        tmp->next = (t_block *)((char *)tmp + small_tblock);
+        tmp->prev = i > 0 ? (t_block *)((char *)tmp - small_tblock) : NULL;
+        tmp = tmp->next;
+    }
+    // LARGE
+    for (i = 0; i < N_BLOCKS; i++) {
+        tmp->size = LARGE;
+        tmp->inuse = false;
+        tmp->next = i < N_BLOCKS - 1 ? (t_block *)((char *)tmp + sizeof(t_block) + LARGE) : NULL;
+        tmp->prev = i > 0 ? (t_block *)((char *)tmp - sizeof(t_block) - LARGE) : NULL;
+        tmp = tmp->next;
+    }
+
+    show_alloc_mem();
+
+    int ret = munmap(g_head, calculate_total_memory());
+    if (ret == -1)
+        return -1;
+    return 0;
+}
+
+
+/// @brief well it's malloc!
+/// @param size bytes to allocate
+/// @return a pointer to memory or NULL in case of failure
+void *malloc(size_t size) {
+    void *ptr = NULL;
+    int page_size = getpagesize();
+    struct rlimit limit;
+
+    prealloc();
+
+    // printf("page size: %d\n", page_size);
+    getrlimit(RLIMIT_AS, &limit);
+    if (size > limit.rlim_cur)
+        return NULL;
 // printf("limit: %lu\n", limit.rlim_cur);
 #ifdef DEBUG
-	printf("Malloc'ing %lu bytes 😜 \n", size);
+    printf("Malloc'ing %lu bytes 😜 \n", size);
 #endif
-	if (size == 0)
-	{
-		size = page_size;
-		ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
-		if (ptr == MAP_FAILED)
-			return NULL;
+    if (size == 0) {
+        size = page_size;
+        ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
+        if (ptr == MAP_FAILED)
+            return NULL;
 #ifdef DEBUG
-		printf("Correctly allocated %lu bytes 😜 \n", size);
+        printf("Correctly allocated %lu bytes 😜 \n", size);
 #endif
-		return ptr;
-	}
-	return malloc(size);
+        return ptr;
+    }
+    return NULL;
 }
 
-void ft_free(void *ptr)
-{
-	free(ptr);
+void free(void *ptr) {
+    (void) ptr;
+    // free(ptr);
 }
 
-void *ft_realloc(void *ptr, size_t size)
-{
-	return realloc(ptr, size);
+void *ft_realloc(void *ptr, size_t size) {
+    return realloc(ptr, size);
 }
