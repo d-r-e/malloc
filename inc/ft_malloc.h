e8d78a31d9ba474d0b5b5305863ddcf3cc7d3095
David Rodríguez Elipe <egoz3ntrum@gmail.com>
2023-10-12 15:21:26 +0200

    Tiny a ocho

--- inc/ft_malloc.h
+++ inc/ft_malloc.h
@@ -48,21 +48,19 @@ enum BlockType {
 
 # define ceil(x) ((x - (int)(x)) > 0 ? (int)(x + 1) : (int)(x))
 
-#define PAGE_SIZE 4096
+# define PAGE_SIZE 4096
 
 # ifndef N_BLOCKS
-#  define N_BLOCKS 100
+#  define N_BLOCKS 128
 # endif
 
 #define OVERHEAD (sizeof(t_block) + ALIGNMENT)
 
-# define TINY (size_t)32
+# define TINY (size_t)8
 # define SMALL (size_t)(TINY * 128)
 
-#define TINY_ARENA (ceil((double)(TINY * N_BLOCKS + OVERHEAD * N_BLOCKS) / PAGE_SIZE) * PAGE_SIZE)
-#define SMALL_ARENA (ceil((double)(SMALL * N_BLOCKS + OVERHEAD * N_BLOCKS) / PAGE_SIZE) * PAGE_SIZE)
-
-
+# define TINY_ARENA (ceil((double)(TINY * N_BLOCKS + OVERHEAD * N_BLOCKS) / PAGE_SIZE) * PAGE_SIZE)
+# define SMALL_ARENA (ceil((double)(SMALL * N_BLOCKS + OVERHEAD * N_BLOCKS) / PAGE_SIZE) * PAGE_SIZE)
 
 # define MMAP_ERROR_STRING "error: mmap failed\n"
 # define MUNMAP_ERROR_STRING "error: received pointer was not properly allocated\n"
@@ -74,15 +72,17 @@ enum BlockType {
 # define PURPLE "\e[1;35m"
 
 
+# define MALLOC_DEBUG_PRINT(...) printf("malloc: debug: " __VA_ARGS__)
+
 extern t_heap g_heap;
 
 /* AUX FUNCTIONS */
-void show_alloc_mem_ex(void);
-void show_alloc_mem(void);
+void	show_alloc_mem_ex(void);
+void	show_alloc_mem(void);
 
 /* SUBJECT FUNCTIONS */
-void *malloc(size_t size);
-void *realloc(void *mem, size_t size);
-void free(void *mem);
+void	*malloc(size_t size);
+void	*realloc(void *mem, size_t size);
+void	free(void *mem);
 
 #endif
